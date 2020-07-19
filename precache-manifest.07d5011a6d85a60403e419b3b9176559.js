074e4c4f85b97878dde31e8043e9b20b0b62dde8
ZIANE-abderrahim <abderrahimziane97@gmail.com>
2020-07-19 11:26:18 +0000

    Updates

---
+++ precache-manifest.07d5011a6d85a60403e419b3b9176559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773f6c2659d1c57e8f5d32093c196076",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "172b595f38500cbf61b6",
+    "url": "/monsters/static/css/main.67a144a2.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172b595f38500cbf61b6",
+    "url": "/monsters/static/js/main.f768d6b3.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
